0072cb16e51e8ff432361a0982687c6dbe08340b
matheusadriel2 <matheusadrieldsc@gmail.com>
2025-01-24 17:20:36 -0300

    feat: one more array exercise

---
+++ udemy/dr-nelio-alves/java/09-comportamento-de-memoria-arrays-e-listas/exercicios/src/main/java/com/exercicios/application/Exercise_12.java
@@ -0,0 +1,34 @@
+package main.java.com.exercicios.application;
+
+import main.java.com.exercicios.entities.TripStudent;
+
+import java.util.Scanner;
+
+public class Exercise_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+        TripStudent[] rooms = new TripStudent[10];
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nRent #" + (i+1) + ":");
+            System.out.print("Name: ");
+            String name = sc.next() + " " + sc.next();
+            sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            rooms[room] = new TripStudent(room, name, email);
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (rooms[i] != null) {
+                System.out.print(rooms[i].toString());
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ udemy/dr-nelio-alves/java/09-comportamento-de-memoria-arrays-e-listas/exercicios/src/main/java/com/exercicios/entities/TripStudent.java
@@ -0,0 +1,33 @@
+package main.java.com.exercicios.entities;
+
+public class TripStudent {
+    private int room;
+    private String name;
+    private String email;
+
+    public TripStudent() {
+    }
+
+    public TripStudent(int room, String name, String email) {
+        this.room = room;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return room + ": " + name + ", " + email + "\n";
+    }
+}
